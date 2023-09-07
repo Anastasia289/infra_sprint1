df4c8fa2c30f30bd1b4b84465756f9c19780d07a
Anastasia <bogdanovanastja@yandex.ru>
2023-09-07 09:40:53 +0000

    2

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -14,7 +14,7 @@ SECRET_KEY = os.getenv('SECRET_KEY', default=utils.get_random_secret_key())
 
 DEBUG = os.environ.get('DEBUG') == 'True'
 
-ALLOWED_HOSTS = ['158.160.15.80', '127.0.0.1', 'localhost', 'procrastination2.hopto.orpto.org']
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS')
 
 
 INSTALLED_APPS = [
